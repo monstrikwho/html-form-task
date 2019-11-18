547eb87362bc2acb73399373d2a74f17b36e952d
dev.tarasiuk <sasha50475@gmail.com>
2019-11-18 22:38:00 +0300

    predeploy

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -14,78 +14,27 @@ window.onload = function() {
             });
         });
 
-//     var x, i, j, selElmnt, a, b, c;
-// /* Look for any elements with the class "custom-select": */
-// x = document.getElementsByClassName("custom-select");
-// for (i = 0; i < x.length; i++) {
-//   selElmnt = x[i].getElementsByTagName("select")[0];
-//   /* For each element, create a new DIV that will act as the selected item: */
-//   a = document.createElement("DIV");
-//   a.setAttribute("class", "select-selected");
-//   a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-//   x[i].appendChild(a);
-//   /* For each element, create a new DIV that will contain the option list: */
-//   b = document.createElement("DIV");
-//   b.setAttribute("class", "select-items select-hide");
-//   for (j = 1; j < selElmnt.length; j++) {
-//     /* For each option in the original select element,
-//     create a new DIV that will act as an option item: */
-//     c = document.createElement("DIV");
-//     c.innerHTML = selElmnt.options[j].innerHTML;
-//     c.addEventListener("click", function(e) {
-//         /* When an item is clicked, update the original select box,
-//         and the selected item: */
-//         var y, i, k, s, h;
-//         s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-//         h = this.parentNode.previousSibling;
-//         for (i = 0; i < s.length; i++) {
-//           if (s.options[i].innerHTML == this.innerHTML) {
-//             s.selectedIndex = i;
-//             h.innerHTML = this.innerHTML;
-//             y = this.parentNode.getElementsByClassName("same-as-selected");
-//             for (k = 0; k < y.length; k++) {
-//               y[k].removeAttribute("class");
-//             }
-//             this.setAttribute("class", "same-as-selected");
-//             break;
-//           }
-//         }
-//         h.click();
-//     });
-//     b.appendChild(c);
-//   }
-//   x[i].appendChild(b);
-//   a.addEventListener("click", function(e) {
-//     /* When the select box is clicked, close any other select boxes,
-//     and open/close the current select box: */
-//     e.stopPropagation();
-//     closeAllSelect(this);
-//     this.nextSibling.classList.toggle("select-hide");
-//     this.classList.toggle("select-arrow-active");
-//   });
-// }
+    // add option for selects
+        var selectDay = document.querySelector('select[name="day"');
+        var selectMonth = document.querySelector('select[name="month"');
+        var selectYear = document.querySelector('select[name="year"');
+        
+        var month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+                
+        
+        for(let i=1; i < 32; i++) {
+            var option = `<option value="${i-1}">${i}</option>`;
+            selectDay.insertAdjacentHTML('beforeend', option);
+        }
 
-// function closeAllSelect(elmnt) {
-//   /* A function that will close all select boxes in the document,
-//   except the current select box: */
-//   var x, y, i, arrNo = [];
-//   x = document.getElementsByClassName("select-items");
-//   y = document.getElementsByClassName("select-selected");
-//   for (i = 0; i < y.length; i++) {
-//     if (elmnt == y[i]) {
-//       arrNo.push(i)
-//     } else {
-//       y[i].classList.remove("select-arrow-active");
-//     }
-//   }
-//   for (i = 0; i < x.length; i++) {
-//     if (arrNo.indexOf(i)) {
-//       x[i].classList.add("select-hide");
-//     }
-//   }
-// }
+        for(let i=1; i < 13; i++) {
+            var option = `<option value="${month[i-1]}">${month[i-1]}</option>`;
+            selectMonth.insertAdjacentHTML('beforeend', option);
+        }
+        
+        for(let i=1980; i < 2012; i++) {
+            var option = `<option value="${i}">${i}</option>`;
+            selectYear.insertAdjacentHTML('beforeend', option);
+        }
 
-// /* If the user clicks anywhere outside the select box,
-// then close all select boxes: */
-// document.addEventListener("click", closeAllSelect);
 }
